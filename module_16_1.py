115e7ed0db65887e1eb3c9d4e50e39c43ab23074
Doctor-Grom666 <127833238+Doctor-Grom666@users.noreply.github.com>
2025-01-24 17:23:11 +0300

    Практика "Основы Fast api и маршрутизация"

---
+++ module_16_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def Get_Main_Page() -> str:
+    return 'Главная страница'
+
+
+@app.get('/user/admin')
+async def Get_Admin_Page() -> str:
+    return 'Вы вошли как администратор'
+
+
+@app.get('/user/{user_id}')
+async def Get_User_Number(user_id) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
+
+
+@app.get('/user/{username}/{age}')
+async def Get_User_Info(username: str, age: int) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
